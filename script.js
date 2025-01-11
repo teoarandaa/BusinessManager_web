76f07fe23aa402185c796f099a3ca77cb90a6ef8
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2025-01-11 01:39:34 +0100

    header done

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     const nav = document.querySelector('nav');
+    const searchButton = document.querySelector('.search-button');
+    const searchInput = document.querySelector('.search-input');
+    const searchClose = document.querySelector('.search-close');
     
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             nav.classList.add('nav-scrolled');
         } else {
             nav.classList.remove('nav-scrolled');
+        }
+    });
+
+    // Funcionalidad del buscador
+    searchButton.addEventListener('click', (e) => {
+        if (!searchButton.classList.contains('active')) {
+            e.preventDefault();
+            searchButton.classList.add('active');
+            searchInput.focus();
+        }
+    });
+
+    // Cerrar con el botón X
+    searchClose.addEventListener('click', (e) => {
+        e.stopPropagation();
+        searchButton.classList.remove('active');
+    });
+
+    // Cerrar el buscador cuando se hace click fuera
+    document.addEventListener('click', (e) => {
+        if (!searchButton.contains(e.target)) {
+            searchButton.classList.remove('active');
+        }
+    });
+
+    // Cerrar el buscador con la tecla Escape
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            searchButton.classList.remove('active');
         }
     });
 }); 
