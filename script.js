9952f2d5ba2fb9380c97b1163a56929977a2bded
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2025-01-13 00:11:19 +0100

    faq section added

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Para los topic buttons en support.html
     const topicButtons = document.querySelectorAll('.topic-button');
-    
     topicButtons.forEach(button => {
         button.addEventListener('click', () => {
-            console.log('Button clicked'); // Para debug
             const wasActive = button.classList.contains('active');
             
             // Cerrar todos los topics
             topicButtons.forEach(btn => {
+                btn.classList.remove('active');
+            });
+            
+            // Si el botón no estaba activo, activarlo
+            if (!wasActive) {
+                button.classList.add('active');
+            }
+        });
+    });
+
+    // Para los faq buttons en faqs.html
+    const faqButtons = document.querySelectorAll('.faq-button');
+    faqButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const wasActive = button.classList.contains('active');
+            
+            // Cerrar todas las FAQs
+            faqButtons.forEach(btn => {
                 btn.classList.remove('active');
             });
             
